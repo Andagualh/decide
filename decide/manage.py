6917e56ac156f7d4f621ee79fb3b96fb92bbe8a9
Andagualh <56022166+Andagualh@users.noreply.github.com>
2021-02-09 18:30:24 +0100

    Update manage.py

--- decide/manage.py
+++ decide/manage.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 #Haciendounpardecomentarios, que bien me lo paso
+#Comentario numero 2, el retorno
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "decide.settings")
