3d32ec560c8930144328d5e1eeafc9d484e2f668
Andagualh <56022166+Andagualh@users.noreply.github.com>
2021-02-11 22:25:17 +0100

    Vagrant Config for WSL
    SSH connection doesn't work if you don't bridge the host PROPERLY, replace config.vm.network host ip or remove.

---
+++ vagrant/provision.sh
@@ -0,0 +1,7 @@
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y git python3 python3-pip screen 
+git clone https://github.com/Andagualh/decide
+cd decide
+pip3 install -r requirements.txt
